678b6c37409f4f6d0094cd7b950aab50e23af162
IrrelevantCube <vrtoxicspringtrap@gmail.com>
2025-12-24 16:06:29 -0700

    The REAL initial commit

---
+++ src/main/java/net/irreleven/seabyss/ShoreEbbAbyssClient.java
@@ -0,0 +1,10 @@
+package net.irreleven.seabyss;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShoreEbbAbyssClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
